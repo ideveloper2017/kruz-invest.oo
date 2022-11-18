193d8ba30ae8fffd1cacce91d8abd8cae8f870c7
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-18 21:31:32 +0500

    commit

--- platform/themes/ripple/functions/functions.php
+++ platform/themes/ripple/functions/functions.php
@@ -4,19 +4,36 @@ use Botble\Blog\Models\Post;
 use Botble\Page\Models\Page;
 
 register_page_template([
-    'no-sidebar' => __('No sidebar'),
+    'default'        => __('Default'),
+    'full'           => __('Full'),
+    'homepage'       => __('Homepage'),
+    'homepage2'      => __('Homepage 2'),
+    'no-breadcrumbs' => __('No Breadcrumbs'),
+    'right-sidebar'  => __('Right sidebar'),
 ]);
 
 register_sidebar([
-    'id'          => 'top_sidebar',
-    'name'        => __('Top sidebar'),
-    'description' => __('Area for widgets on the top sidebar'),
+    'id'          => 'footer_sidebar_1',
+    'name'        => __('Footer sidebar 1'),
+    'description' => __('Sidebar in the footer of page'),
 ]);
 
 register_sidebar([
-    'id'          => 'footer_sidebar',
-    'name'        => __('Footer sidebar'),
-    'description' => __('Area for footer widgets'),
+    'id'          => 'footer_sidebar_2',
+    'name'        => __('Footer sidebar 2'),
+    'description' => __('Sidebar in the footer of page'),
+]);
+
+register_sidebar([
+    'id'          => 'footer_sidebar_3',
+    'name'        => __('Footer sidebar 3'),
+    'description' => __('Sidebar in the footer of page'),
+]);
+
+register_sidebar([
+    'id'          => 'footer_sidebar_4',
+    'name'        => __('Footer sidebar 4'),
+    'description' => __('Sidebar in the footer of page'),
 ]);
 
 RvMedia::setUploadPathAndURLToPublic();
