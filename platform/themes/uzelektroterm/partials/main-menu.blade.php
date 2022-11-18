84f48fd20f60b79671f2cb7b418f1012ae6b1425
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-18 10:43:38 +0500

    commit

--- platform/themes/uzelektroterm/partials/main-menu.blade.php
+++ platform/themes/uzelektroterm/partials/main-menu.blade.php
@@ -1,6 +1,6 @@
 <ul {!! clean($options) !!}>
     @foreach ($menu_nodes as $key => $row)
-        <li class="{!! (!$row->parent_id)?"dropdown":""!!} {{ $row->css_class }} @if ($row->active) active @endif">
+        <li class="{!! (!$row->parent_id)?"dropdown":""!!}{{ $row->css_class }} @if ($row->active) active @endif">
             <a href="{{ $row->url }}" target="{{ $row->target }}">
                 @if ($row->icon_font)<i class='{{ trim($row->icon_font) }}'></i> @endif{{ $row->title }}
 
