7676a2fe84dceab33674724a47eee6da6f555e11
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-24 23:30:55 +0500

    commit

--- platform/themes/uzelektroterm/partials/breadcrumbs.blade.php
+++ platform/themes/uzelektroterm/partials/breadcrumbs.blade.php
@@ -3,24 +3,21 @@
         <div class="page-title-banner">
             <div class="container">
                 <div class="content-wrapper">
-                    <h2>{{ Theme::get('section-name') }}</h2>
-                    <ul class="bread-crumb">
-                        @foreach (Theme::breadcrumb()->getCrumbs() as $i => $crumb)
-                            @if ($i != (count(Theme::breadcrumb()->getCrumbs()) - 1))
-                                <li><a href="#"><i class="icofont icofont-home"></i></a></li>
-                                <li><a href="{{ $crumb['url'] }}">{!! $crumb['label'] !!}</a></li>
-                            @else
-                                <li><a href="{{ $crumb['url'] }}">{!! $crumb['label'] !!}</a></li>
+                    <h2>Latest Blog</h2>
+                    <u  @foreach (Theme::breadcrumb()->getCrumbs() as $i => $crumb)
+                        @if ($i != (count(Theme::breadcrumb()->getCrumbs()) - 1))
+                            <li><a href="#"><i class="icofont icofont-home"></i></a></li>
+                            <li><a href="{{ $crumb['url'] }}">{!! $crumb['label'] !!}</a></li>
+                        @else
+                            <li><a href="{{ $crumb['url'] }}">{!! $crumb['label'] !!}</a></li>
 
                             @endif
-                        @endforeach
-{{--                        <li><a href="#">Home</a></li>--}}
-{{--                        <li><a href="#">Blog</a></li>--}}
+                            @endforeach
                     </ul>
                 </div> <!-- .content-wrapper -->
             </div> <!-- .container -->
         </div> <!-- .page-title-banner -->
-    </section>
+    </section> <!-- .hero-area -->
 
 <section class="breadcrumbs-wrap" style="display: none">
     <div class="container">
