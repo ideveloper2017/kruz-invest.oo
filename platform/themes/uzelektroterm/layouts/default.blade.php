09d61b400fc61c8b3efed6c94e98bf4411605b17
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-26 03:59:58 +0500

    commit

--- platform/themes/uzelektroterm/layouts/default.blade.php
+++ platform/themes/uzelektroterm/layouts/default.blade.php
@@ -1,4 +1,20 @@
 {!! Theme::partial('header') !!}
 {!! Theme::partial('breadcrumbs') !!}
-{!! Theme::content() !!}
+<div id="body-content">
+    <div class="sidebar-overflow">
+        <div class="container">
+            <div class="row pad-top">
+                <div class="col-md-12 col-lg-8">
+                    <div class="sidebar-padding">
+                    {!! Theme::content() !!}
+                    </div>
+                </div>
+                <div class="col-md-12 col-lg-4 sidebar-left order-lg-first">
+                    <aside class="sidebar-spacer row">
+                    {!! dynamic_sidebar('primary_sidebar') !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 {!! Theme::partial('footer') !!}
