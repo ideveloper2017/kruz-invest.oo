eaad5e2ae62b321dc56cee15b2c8d34394dcee5c
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-26 03:25:40 +0500

    commit

--- platform/themes/uzelektroterm/partials/shortcodes/sliders/main.blade.php
+++ platform/themes/uzelektroterm/partials/shortcodes/sliders/main.blade.php
@@ -1,41 +1,26 @@
+@if (is_plugin_active('simple-slider') && count($sliders) > 0 &&
+    $sliders->loadMissing('metadata') && $slider->loadMissing('metadata'))
+    @php
+        $style = $slider->getMetaData('simple_slider_style', true);
+        echo $style;
+    @endphp
+    @if ($style == 'style-3')
+
 <section class="hero-area">
     <div id="corporex-slider" class="corporex-slider corporex-slider-02 carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#corporex-slider" data-slide-to="0" class="active"></li>
             <li data-target="#corporex-slider" data-slide-to="1"></li>
         </ol> <!-- .carousel-indicators -->
-
+        @foreach($sliders as $slider)
         <div class="carousel-inner" role="listbox">
             <div class="item caption-center active">
-                <img class="slider-bg img-responsive" src="http://via.placeholder.com/1920x720" alt="slider image 01">
-                <div class="container">
+                <img class="slider-bg img-responsive" src="{{ RvMedia::getImageUrl($slider->image, null, false, RvMedia::getDefaultImage()) }}" alt="slider image 01">
 
-                    <div class="carousel-caption">
-                        <h1 class="h1-extra"><span>The Corporex</span>Corporex will help you to grow</h1>
-                        <p class="lead">
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse
-                            cillum dolore eu fugiat nulla pariatur.
-                        </p>
-                        <a class="btn btn-main" href="#">learn more</a>
-                    </div> <!-- .carousel-caption -->
-                </div> <!-- .container -->
             </div> <!-- .item -->
-            <div class="item caption-left gradient-left-light">
-                <img class="slider-bg img-responsive" src="http://via.placeholder.com/1920x720" alt="slider image 02">
-                <div class="container">
 
-                    <div class="carousel-caption">
-                        <h1 class="h1-extra"><span>corporex Theme</span>Feel the power of Corporex</h1>
-                        <p class="lead">
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse
-                            cillum dolore eu fugiat nulla pariatur.
-                        </p>
-                        <a class="btn btn-main" href="#">learn more</a>
-                    </div> <!-- .carousel-caption -->
-                </div> <!-- .container -->
-            </div> <!-- .item -->
         </div> <!-- .carousel-inner -->
-
+        @endforeach
         <!-- Controls -->
         <a class="left carousel-control" href="#corporex-slider" role="button" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
